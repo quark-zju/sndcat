7f39137db244c2b7e931013692db5dc5f5a4c525
Jun Wu <quark@lihdd.net>
2021-04-10 18:40:22 -0700

    vcpkg: add portaudio build script
    Easier to build portaudio.

---
+++ vcpkg-ports/build-portaudio.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import subprocess
+
+
+def main():
+    vcpkg_root = os.getenv("VCPKG_ROOT")
+    if not vcpkg_root:
+        sys.stderr.write("VCPKG_ROOT is not set. Is vcpkg installed?")
+        return 1
+    subprocess.check_call(
+        [
+            os.path.join(vcpkg_root, "vcpkg"),
+            "--overlay-ports=%s" % os.getcwd(),
+            "install",
+            "portaudio:x64-windows",
+        ]
+    )
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main() or 0)
