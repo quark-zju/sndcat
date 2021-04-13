979368648caacc06c7a0eda82f581e1c3a368835
Jun Wu <quark@lihdd.net>
2021-04-12 21:35:37 -0700

    Update build-portaudio.bat

--- vcpkg-ports/build-portaudio.bat
+++ vcpkg-ports/build-portaudio.bat
@@ -1,4 +1,9 @@
+@echo off
+
 setlocal
 cd /d %~dp0
+
+if not defined VCPKG_ROOT if defined VCPKG_INSTALLATION_ROOT set VCPKG_ROOT=%VCPKG_INSTALLATION_ROOT%
+if not defined VCPKG_ROOT echo vcpkg seems missing?
 
 %VCPKG_ROOT%\vcpkg --overlay-ports=%CD% install portaudio:x64-windows-static-md
